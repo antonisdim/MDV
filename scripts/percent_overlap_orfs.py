6e4fcd166e579683a8174e0bf314ff006f0aa7e8
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2021-02-18 15:29:03 +0000

    New script and up to concatenating all the coding sequences

---
+++ scripts/percent_overlap_orfs.py
@@ -0,0 +1,31 @@
+from Bio import SeqIO
+from sys import argv
+
+script = argv[0]
+input_files = argv[1:]
+
+files_to_keep = []
+
+for file in input_files:
+
+	record_nucleotide_dict = {}
+
+	for record in SeqIO.parse(file, "fasta"):
+		A = record.seq.count('A')
+		C = record.seq.count('C')
+		T = record.seq.count('T') 
+		G = record.seq.count('G') 
+		bases_covered = A + C + T + G
+		record_nucleotide_dict[record.id] = bases_covered / float(len(record.seq))
+		print(record.id, len(record.seq), sep=' ')
+		print('A:', A, 'T:', T, 'C:', C, 'G:', G)
+
+	print(record_nucleotide_dict)
+
+	if all(value >= 0.10 for value in record_nucleotide_dict.values()):
+		files_to_keep.append(file)
+
+with open('selected_orfs.txt', 'w') as fout:
+	print("\n".join(files_to_keep), file=fout)
+
+print(len(files_to_keep))
